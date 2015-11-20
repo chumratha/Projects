b10f5a227e40045fa30add72c884a49b0a82146c
Korea Software HRD Center <koreasoftwarehrdcenter@ratha.local>
2015-11-20 15:07:01 +0700

    Initial Commit

---
+++ IosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IosTest
+//
+//  Created by Korea Software HRD Center on 11/20/15.
+//  Copyright © 2015 Korea Software HRD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IosTest
+//
+//  Created by Korea Software HRD Center on 11/20/15.
+//  Copyright © 2015 Korea Software HRD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
